5620b8287b25af6ba44ddb7e5fded6038368af3e
Claudius Röhl <claudius@roehlnet.de>
2025-09-12 20:17:21 +0200

    Fixed warnings.

--- src/tsp.rs
+++ src/tsp.rs
@@ -2,4 +2,3 @@ mod tsp_instance;
 mod tsp_solution;
 
 pub use tsp_instance::TSPInstance;
-pub use tsp_solution::TSPSolution;
--- src/tsp/tsp_solution.rs
+++ src/tsp/tsp_solution.rs
@@ -1,5 +1,4 @@
 use std::collections::HashSet;
-use std::hash::Hash;
 use std::sync::Arc;
 
 use super::TSPInstance;
