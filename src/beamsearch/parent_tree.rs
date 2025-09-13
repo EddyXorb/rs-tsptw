58db1849006190305cbba01a8a5844dca92c6da9
Claudius Röhl <claudius@roehlnet.de>
2025-09-13 20:20:38 +0200

    Added test that nodes are deleted as soon as last leaf is not existent.

--- src/beamsearch/parent_tree.rs
+++ src/beamsearch/parent_tree.rs
@@ -50,6 +50,8 @@ impl<T> ParentTreeNode<T> {
 
 #[cfg(test)]
 mod tests {
+    use std::rc::Rc;
+
     use super::*;
 
     #[test]
@@ -92,6 +94,7 @@ mod tests {
     #[test]
     fn bigger_tree_works() {
         let root = ParentTreeNode::new_root(1);
+
         let mut child = root;
         for i in 2..=10 {
             let _child = child.new_child(i * 100);
@@ -102,6 +105,29 @@ mod tests {
 
         assert_eq!(ancestors.len(), 10);
         assert_eq!(ancestors, (1..=10).rev().collect::<Vec<i32>>());
+    }
+
+    #[test]
+    fn unused_nodes_are_effectively_deleted() {
+        //Acts as counter for nodes
+        let root_counter = Rc::new(0);
+        let child_counter = Rc::new(0);
+
+        let root = ParentTreeNode::new_root(root_counter.clone());
+
+        {
+            let mut child = root;
+
+            for _i in 1..10 {
+                child = child.new_child(child_counter.clone());
+            }
+
+            assert_eq!(Rc::strong_count(&root_counter), 2);
+            assert_eq!(Rc::strong_count(&child_counter), 10);
+        }
+
+        assert_eq!(Rc::strong_count(&root_counter), 1);
+        assert_eq!(Rc::strong_count(&child_counter), 1);
     }
 
     #[test]
